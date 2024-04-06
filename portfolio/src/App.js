af1babe48b985f7ec01ccfdf1fb8ff26f6e4fdb4
Parker Ciaramella <parkeraciaramella@gmail.com>
2024-04-06 16:38:52 -0400

    install mui, box-shadow div

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,7 @@
-import choppa from './choppa.svg';
 import './App.css';
 import './fonts.css';
 import Sidebar from './Sidebar'; // Import your Sidebar component
+import { Grid } from '@mui/material';
 
 function App() {
   return (
@@ -9,10 +9,18 @@ function App() {
        <link href="./output.css" rel="stylesheet"></link>
       <Sidebar />
       <body className="App-header">
-      {/* <img src={choppa} className="pulse" alt="logo" /> */}
-        <p>
-          Let's Adventure!
-        </p>
+        <Grid container spacing={2}>
+          <Grid item xs={7}>
+            <p>
+             Let's Adventure!
+            </p>
+          </Grid>
+          <Grid item xs={4}>
+            <div className='App-thick-div'>
+              This is real. This is me. This is exactly where I'm supposed to be.
+            </div>  
+          </Grid>
+        </Grid>
       </body>
     </div>
   );
